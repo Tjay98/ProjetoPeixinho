f3dd43bdd4e5f647fe5fbec58d78b8eba0d93c8e
Rodolfo Miguel Esperança Dos Santos Barreira <2180714@my.ipleiria.pt>
2019-05-07 12:44:14 +0100

    Adicionar script, css e weblogic

---
+++ Projeto Peixinho/Pages/weblogicmvc/webapp/router.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smendes
+ * Date: 02-05-2016
+ * Time: 11:18
+ */
+use ArmoredCore\Facades\Router;
+
+/****************************************************************************
+ *  URLEncoder/HTTPRouter Routing Rules
+ *  Use convention: controllerName@methodActionName
+ ****************************************************************************/
+
+Router::get('/',			'HomeController/index');
+Router::get('home/',		'HomeController/index');
+Router::get('home/index',	'HomeController/index');
+Router::get('home/start',	'HomeController/start');
+
+
+
+
+
+
+
+
+
+
+
+/************** End of URLEncoder Routing Rules ************************************/
